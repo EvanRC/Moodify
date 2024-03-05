90af4729d07e1dca43d979cd7bcf85d976f38882
Clayton Hensley <chensley56@gmail.com>
2024-03-04 20:26:44 -0800

    added GetStartedBtn and Menu with genre drop down, no functionality yet

---
+++ client/src/components/GetStartedBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GetStartedBtn = ({ onClick }) => {
+  return (
+    <button onClick={onClick}>Let's Get Started!</button>
+  );
+};
+
+export default GetStartedBtn;
--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import GetStartedBtn from './GetStartedBtn';
 
 const Success = () => {
-  return (
-    <div className="success-container">
+    const navigate = useNavigate();
+    const handleGetStarted = () => {
+        navigate('/Menu')
+    }
+
+    return (
+      <div className="success-container">
       <h1>Spotify Authentication Successful!</h1>
       <p>You have successfully authenticated with Spotify.</p>
+      <GetStartedBtn onClick={handleGetStarted} />
     </div>
   );
 };
