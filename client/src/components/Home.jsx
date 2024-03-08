8494b15409847ca8d2d5d7532dfd8a83f54d2c9b
Evan Carlson <evanrc1@gmail.com>
2024-03-07 19:42:41 -0800

    added tsparticles to add some flare to page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import LoginBtn from './LoginBtn'; 
+import LoginBtn from './LoginBtn';
 import Layout from './layout';
 import Menu from './Menu'
 
+
 const Home = () => {
   return (
-    
     <div className="home-container">
       <h1 className='button-header'>Login with spotify to get Started!</h1>
       <LoginBtn />
